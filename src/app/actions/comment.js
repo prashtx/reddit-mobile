b714ad804a2f349aaff8697cee8e7b3d08f1556a
Jack Lawson ajacksified@gmail.com <ajacksified@gmail.com>
2016-05-27 11:13:58 -0700

    Send new comment in comment.replied action

--- src/app/actions/comment.js
+++ src/app/actions/comment.js
@@ -56,3 +56,44 @@ export const LOADED_MORE = 'LOADED_MORE';
 export const loadMore = ids => async (dispatch, getState) => {
   console.log(ids, getState());
 };
+
+export const REPLYING = 'REPLYING';
+export const REPLIED = 'REPLIED';
+export const REPLY = 'REPLY';
+
+export const replying = (id, text) => ({ type: REPLYING, id, text });
+export const replied = (newComment) => ({ type: REPLIED, comment: newComment });
+export const reply = (id, text) => async (dispatch, getState) => {
+  const state = getState();
+
+  if (state.replying[id] || !text || !id) { return; }
+
+  text = text.trim();
+
+  const type = models.ModelTypes.thingType(id);
+  const objects = state[`${type}s`];
+  const thing = objects[id];
+
+  dispatch(replying(id, text));
+
+  const apiResponse = await thing.reply(apiOptionsFromState(state), text);
+  const newRecord = apiResponse.results[0];
+
+  // add the new model
+  dispatch(receivedResponse(apiResponse, models.ModelTypes.COMMENT));
+
+  // If it's a comment, stub the parent so it receives the new replies.
+  if (type === models.ModelTypes.COMMENT) {
+    const stub = thing.set({ replies: [newRecord, ...thing.replies] });
+
+    // update the parent with the new reply
+    dispatch(updatedModel(stub, type));
+
+  } else if (type === models.ModelTypes.POST) {
+    const comment = apiResponse.comments[apiResponse.results[0].uuid];
+    // If it's a post, add it as a new model.
+    dispatch(newModel(comment, models.ModelTypes.COMMENT));
+  }
+
+  dispatch(replied(apiResponse.comments[newRecord.uuid]));
+};
